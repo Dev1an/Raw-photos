618c1fa7625e2b9441948c96909cb91ae00d23e9
Damiaan Dufaux <damiaan@dpro.be>
2016-11-24 10:30:07 +0100

    Initial Commit

---
+++ Camera Raw/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Camera Raw
+//
+//  Created by Damiaan on 24-11-16.
+//  Copyright © 2016 Damiaan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Camera Raw/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Camera Raw
+//
+//  Created by Damiaan on 24-11-16.
+//  Copyright © 2016 Damiaan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
